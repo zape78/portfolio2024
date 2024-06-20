760e09505e3ebd28303b6a1dd07849ccf285f0bd
Brian Zapien <brianzapien@gmail.com>
2024-06-19 21:42:58 -0700

    Added FloatingNav Component to ui Folder

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -33,7 +33,7 @@ const Hero = () => {
 
           <TextGenerateEffect
             className="text-center text-[40px] md:text-5xl lg:text-6xl"
-            words="Transforming Concepts into Seemless Experiences"
+            words="Transforming Concepts into Seemless User Experiences"
           />
           <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
             Hi. I&apos;m Brian. I&apos;m an aspiring developer!
